83d77f56cad910038675730de2b31de45eb07411
romeroluisraul <luisr.romeroa@gmail.com>
2022-11-15 09:32:14 -0300

    Modificaciones de estilo. Se guarda para aplicar herencia multiple de templates en distintos niveles.

--- parques_nacionales/views.py
+++ parques_nacionales/views.py
@@ -1,17 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from datetime import date
+
+contexto_global = {'cantidad': 17, 'fecha': date.today().strftime("%a %B %d, %Y"),
+                'anio' : str(date.today().year),
+                'title_description':'La Argentina cuenta con 38 Parques Nacionales únicos en el mundo. '}
 
 def home(request):
 
     plantilla = loader.get_template('home.html')
-    documento = plantilla.render()
+    contexto_global['home_url'] = request.get_full_path() == '/home/'
+    documento = plantilla.render(contexto_global)
 
     return HttpResponse(documento)
 
 def explorando(request):
 
-    plantilla = loader.get_template('base.html')
-    documento = plantilla.render()
+    plantilla = loader.get_template('post.html')
+    contexto_global['home_url'] = request.get_full_path() == '/home/'
+    documento = plantilla.render(contexto_global)
 
     return HttpResponse(documento)
